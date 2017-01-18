8ad24a80362d52ae5a9b921907944fb6a8b678cb
jaurimfl <jaurimfl@gmail.com>
2017-01-18 20:38:21 +0900

    remove empty word

---
+++ knowEasy/knowEasy/strexample1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char word[100] = "It is useful Where a statement Is needed sYntactically, but no action is reQUired";
+	char *p;
+	int i = 0; 
+	int j = 0;
+	p = &word;
+
+	for (i=0, j =0; i < strlen(word); i++)
+	{
+		if (p[i] == ' ')
+			continue;
+		p[j] = p[i];
+		j++;
+	}
+	
+	p[j] = '\0';
+	printf("%s", word);
+
+	return 0;
+}
