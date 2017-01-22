db683c02e2bf959e9efae45b164815d3122c1f51
jaurimfl <jaurimfl@gmail.com>
2017-01-23 02:32:31 +0900

    mystrlen function

---
+++ knowEasy/knowEasy/mystrlen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int myStrlen(char *str);
+
+int main()
+{
+	char name[100];
+	char *p;
+	int lenght;
+	p = &name;
+
+	scanf("%s", name);
+
+	lenght = myStrlen(p);
+
+	printf("%d\n", lenght);
+	printf("%d\n", strlen(name));
+
+	return 0;
+}
+
+int myStrlen(char *str)
+{
+	int lenght = 0;
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (str[i] != '\0')
+			lenght++;
+		else if (str[i] == '\0')
+			break;
+	}
+	return lenght;
+}
